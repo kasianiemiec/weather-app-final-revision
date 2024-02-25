593ce30bbc38e69f082423feec9b72b35f3054af
kasianiemiec <kasia.a.niemiec@gmail.com>
2024-02-26 00:18:30 +0200

    added weather data

--- src/script.js
+++ src/script.js
@@ -1,9 +1,53 @@
+function refreshWeather(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#current-wind");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let timeElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "98bc9f158331o422ta5db05dbd90ba36";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
